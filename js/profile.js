4e8faa178c25e404246e574baebe145e4ac73504
GageBerz <gageberz@protonmail.ch>
2021-01-19 14:26:35 -0800

    added new_image and commented out test code

--- js/profile.js
+++ js/profile.js
@@ -1,15 +1,16 @@
 $(document).ready(
     function() {
 
-        new_name = 'test';
+        var new_name = 'test';
+        var new_image = 'test';
 
         $('.apply-settings').click( 
             function() {
                 localStorage.setItem('username', new_name);
                 localStorage.setItem('image', new_image);
 
-                $('.name').replaceWith($('<p class="name">' + localStorage.getItem(username) + '</p>'));
-                $('.avatar').attr('src', $(new_image).attr('src'));
+                //$('.name').replaceWith($('<p class="name">' + localStorage.getItem(username) + '</p>'));
+                //$('.avatar').attr('src', $(new_image).attr('src'));
         });
 
 
