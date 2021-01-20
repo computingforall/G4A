90fb8814bc37ce9744eb23f6b04ee7607dd51f81
Raymond Nhim <raymondnh@function-HotSoz.localdomain>
2021-01-20 15:18:50 -0800

    Fixed like button

--- js/global.js
+++ js/global.js
@@ -1,37 +1,36 @@
-$(document).ready(
-    function() {
-
-        // Main Navigation Bar
-        var nav_html =
-        `
+$(document).ready(function () {
+  // Main Navigation Bar
+  var nav_html = `
         <header>
             <div><div class="nav-logo">Games For All</div></div>
             <nav>
                 <a></a>
             </nav>
         </header>
-        `
+        `;
 
-        var footer_html =
-        `
+  var footer_html = `
         <footer>
             <div id="copyright"><i class="fas fa-copyright"></i> Games For All 2021</div>
             <nav></nav>
         </footer>
-        `
+        `;
 
-        $('body').prepend(nav_html);
-        $('body').append(footer_html);
+  $("body").prepend(nav_html);
+  $("body").append(footer_html);
 
-        let nav_items = $('nav').find('a').first();
-        nav_items.clone().appendTo('nav').attr('href','#').html('Home');
-        nav_items.clone().appendTo('nav').attr('href','#').html('Games');
-        nav_items.clone().appendTo('nav').attr('href','#').html('Profile & Settings');
-        $('nav').find('a').first().remove();
+  let nav_items = $("nav").find("a").first();
+  nav_items.clone().appendTo("nav").attr("href", "#").html("Home");
+  nav_items.clone().appendTo("nav").attr("href", "#").html("Games");
+  nav_items
+    .clone()
+    .appendTo("nav")
+    .attr("href", "#")
+    .html("Profile & Settings");
+  $("nav").find("a").first().remove();
 
-        //Comments Field 
-        var commentTemplate =
-        `
+  //Comments Field
+  var commentTemplate = `
         <div class="post">
              <div class="user-pro"> 
                 <div><img src="./img/shiggy.jpg"></div>
@@ -40,38 +39,47 @@ $(document).ready(
                 <div><p class="comment">This is a comment.</p></div>
                 <div class="likeButton"></div>
         </div>
+        `;
+
+  // $('.posts').append(commentTemplate);
+  $(".comment-submit").click(function () {
+    let text = $("#comment-text").val();
+    var commentTemplate =
         `
-        
-        // $('.posts').append(commentTemplate);
-        $('.comment-submit').click(
-            function () {
-                let text = $('#comment-text').val();
-                var commentTemplate =
-                `
                 <div class="post">
                      <div class="user-pro"> 
                         <div><img src="./widgets/img/shiggy.jpg"></div>
                         <div><h2>Player 1</h2></div>
                     </div>
-                        <div><p class="comment">`+text+`</p></div>
+                        <div><p class="comment">` +text + `</p></div>
                         <div></div>
-                        <div class="likeButton"></div>
+                        <div class=''><p class="likeCount">0</p><i class='like unliked far fa-heart'></i></div>
                 </div>
-                `
-                $('.posts').append(commentTemplate);
-                $('#comment-text').val('');
+        `;
+    $(".posts").append(commentTemplate);
+    $("#comment-text").val("");
+  });
 
-        });
+  //Like Dislike Buttons
+  // Create value and add to element, function updates value, updates element
+  $(".like").click(function () {
+    // console.log(typeof $('.like').html())
+    let likeBon = $(".like");
+    let likes = $(".likeCount").text();
+    let likeNum = parseInt(likes);
+    if ($(likeBon).hasClass("liked")) {
+      likeNum--;
+      $(".like").removeClass("liked");
+      $(".like").addClass("unliked");
+      $(".likeCount").html(likeNum);
+      return;
+    }
 
-        //Like Dislike Buttons
-        // Create value and add to element, function updates value, updates element
-        $('.like').click(
-            function() {
-            // console.log(typeof $('.like').html())
-                let likes = $('.like').text();
-                let likeCount = parseInt(likes);
-                likeCount++;
-                $('.like').html(likeCount);
-            }
-        )
+    if ($(likeBon).hasClass("unliked")) {
+      likeNum++;
+      $(".like").removeClass("unliked");
+      $(".like").addClass("liked");
+    }
+    $(".likeCount").html(likeNum);
+  });
 });
