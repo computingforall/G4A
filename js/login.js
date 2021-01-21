30bfe2b98c5c293afaa628c8c14e04eca189385c
GageBerz <gageberz@protonmail.ch>
2021-01-21 14:55:27 -0800

    login compares to registration user/pass. does nothing if don't match.

--- js/login.js
+++ js/login.js
@@ -1,5 +1,24 @@
 $(document).ready(
     function() {
         
+        var loginTemplate =
+        `
+        <form>
+            <label for="username">Username: </label><br>
+            <input type="text" name="username" id="username" required><br>
 
+            <label for="password">Password: </label><br>
+            <input type="password" name="password" id="password" required><br>
+
+            <input type="submit" value="Submit" id="submit-login">
+        <form>
+        `
+        $(loginTemplate).prependTo('body');
+        $('#submit-login').click(
+            function() {
+                if(($('#password').val() == localStorage.getItem('password')) && ($('#username').val() == localStorage.getItem('username'))) {
+                    alert("Successfully logged in");
+                }
+            }
+        );
 });
