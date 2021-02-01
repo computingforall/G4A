2466a067f76b416afa6b687e3c0adc4b17d2ba4c
GageBerz <gageberz@protonmail.ch>
2021-02-01 14:43:24 -0800

    removed old code

--- js/profile.js
+++ js/profile.js
@@ -4,12 +4,9 @@ $(document).ready(
         $('.apply-settings').click( 
             function(e) {
                 e.preventDefault();
-                
+
                 localStorage.setItem('username', $('#username').val());
                 localStorage.setItem('image', $('#image').val());
-
-                //$('.name').replaceWith($('<p class="name">' + localStorage.getItem(username) + '</p>'));
-                //$('.avatar').attr('src', $(new_image).attr('src'));
 
         });
 
