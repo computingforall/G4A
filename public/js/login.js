7db57ba25dd9188bfd67470846ec77d1307ef54a
GageBerz <gageberz@protonmail.ch>
2021-02-17 13:55:29 -0800

    login/global changes

--- public/js/login.js
+++ public/js/login.js
@@ -1,15 +1,22 @@
 $(document).ready(
     function() {
+
+        const displayname_regex = /^(?=.*[\w!@#$%^&*()_-]).{4,16}$/;
+        const email_regex = /^(("[\w\d-.!%+ ]{1,64}")|^([\w\d-.!%+]{1,64}))(@[a-zA-Z0-9-.]+)(.[\w\d]+)?$/;
+        const password_regex = /^(?=.*[\w])(?=.*[!@#$%^&*()_-]).{6,16}$/; // word, must have some special character, 6 chars min for length, 16 max.
+
+
+        // LOGIN
         var loginTemplate = 
         `
         <form name="login-form" id="login-form">
-            <label for="email">email: </label><br>
+            <label for="email">Email: </label><br>
             <input type="text" name="email" id="email" required><br>
 
             <label for="password">Password: </label><br>
             <input type="password" name="password" id="password" required><br>
 
-            <input type="submit" value="Submit" id="submit-login">
+            <input type="submit" value="Login" id="submit-login">
         </form>
         `;
 
@@ -20,16 +27,71 @@ $(document).ready(
             const email = $('#email').val();
             const password = $('#password').val();
 
-            axios.post('/login', {
-                email,
-                password
-            })
-              .then((response) => {
-                  window.location = '/';
-              })
-              .catch((error) => {
-                  console.log(error);
-              });
+            if (email_regex.exec(email) === null) {
+                alert('bad email');
+            } else if (password_regex.exec(password) === null) {
+                alert('bad password');
+            } else {
+                axios.post('/login', {
+                    email,
+                    password
+                })
+                .then((response) => {
+                    window.location = '/';
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            }
+        });
+
+        // REGISTER
+        var registrationTemplate =
+        `
+        <form name="form" id="register-form">
+            <label for="displayname">Display Name: </label><br>
+            <input type="text" name="displayname" id="displayname" required><br>
+
+            <label for="email">Email: </label><br>
+            <input type="text" name="email" id="new-email" required><br>
+
+            <label for="password">Password: </label><br>
+            <input type="password" name="password" id="new-password" required><br>
+            <div>
+            <label for="tos">Agree to <a href="#">Terms of Service</a>:</label>
+            <input type="checkbox" name="tos" id="tos" required><br>
+            </div>
+            <input type="submit" id="submit-registration" value="Register">
+        </form>
+        `
+        $(registrationTemplate).appendTo('#register');
+
+
+        $('#register-form').on('submit', function(e) {
+            e.preventDefault();
+            const displayname = $('#displayname').val();
+            const email = $('#new-email').val();
+            const password = $('#new-password').val();
+
+            if (email_regex.exec(email) === null) {
+                alert('bad email');
+            } else if (password_regex.exec(password) === null) {
+                alert('bad password');
+            } else if (displayname_regex.exec(displayname) === null) {
+                alert('bad display name');
+            } else {
+                axios.post('/register', {
+                    displayname,
+                    email,
+                    password
+                })
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+            } 
         });
 
 });
