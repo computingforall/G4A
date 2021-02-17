62702f47db173dcb7492e3ea18f9ce39159bf7d4
deconile <deconile@users.noreply.github.com>
2021-02-16 18:56:07 -0800

    newsfeed styles updates

--- public/js/global.js
+++ public/js/global.js
@@ -92,10 +92,10 @@ $(document).ready(
                         </div>
                     </div>
                     <div id=${commentId} class="reply-box"></div>
-                    <button class="reply-button">Reply</button>
+                    <div class="reply-button-container"><button class="reply-button">Reply</button></div>
                     <div class="reply-field">
+                        <textarea class="reply-text" placeholder="Leave a reply..."></textarea>
                         <button class="reply-submit">Submit</button>
-                        <textarea class="reply-text"></textarea>
                     </div>
                 </div>
                 `
@@ -107,7 +107,7 @@ $(document).ready(
                 $('.reply-field').eq(commentId -1).hide();
                 $('.reply-button').click(
                     function () {
-                        $(this).siblings('.reply-field').show();
+                        $(this).parent().siblings('.reply-field').show();
                     }
                 );
                 
@@ -128,17 +128,23 @@ $(document).ready(
                       
                         var replyTemplate = 
                         `
-                        <div><p>` + reply + `</p></div>
-                        <div class="pointButton likeButton">
-                        <div class="pointButtonG like unliked checkLike">
-                            <i class="fas fa-thumbs-up"></i>
-                            <p class="likeCount">0</p>
+                        <div class="reply-post">
+                            <div class="user-pro"> 
+                                <div><img src='${localStorage.getItem('image')}'></div>
+                                <div><h2>`+ session_data + ` </h2></div>
+                            </div>
+                            <div><p>` + reply + `</p></div>
+                            <div class="pointButton likeButton">
+                                <div class="pointButtonG like unliked checkLike">
+                                    <i class="fas fa-thumbs-up"></i>
+                                    <p class="likeCount">0</p>
+                                </div>
+                                <div class="pointButtonG dislike undisliked checkDislike">
+                                    <i class="fas fa-thumbs-down"></i>
+                                    <p class="dislikeCount">0</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="pointButtonG dislike undisliked checkDislike">
-                            <i class="fas fa-thumbs-down"></i>
-                            <p class="dislikeCount">0</p>
-                        </div>
-                    </div>
                         `
 
                         $('#' + replyId).append(replyTemplate);
